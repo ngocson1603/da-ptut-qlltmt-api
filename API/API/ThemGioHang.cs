beef51eaa8d1b76e30c21993266510990eb4bfa0
ngocson1603 <ngocson16032001@gmail.com>
2022-05-15 21:13:45 +0700

    themgioapi

---
+++ API/API/ThemGioHang.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API
+{
+    public class ThemGioHang
+    {
+        public int MaHoaDon { get; set; }
+        public int MaSanPham { get; set; }
+        public string Gmail { get; set; }
+        public int SoLuong { get; set; }
+        public int TongTien { get; set; }
+        public int TongTienHoaDon { get; set; }
+        public DateTime NgayLapHoaDon { get; set; }
+        public int MaNhanVien { get; set; }
+    }
+}
